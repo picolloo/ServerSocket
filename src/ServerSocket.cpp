7c7367de2f7b6f2705d12bfb96e54cfd76fcedce
Picollo <lucaspicolloo@gmail.com>
2018-06-27 22:20:21 -0300

    Server working but no client to test

--- src/ServerSocket.cpp
+++ src/ServerSocket.cpp
@@ -1,18 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <fstream>
 #include <string.h>
 #include <iostream>
+#include <unistd.h>
 
 #define PORT 8080
 
-struct sockaddr_in serv;
+struct sockaddr_in serv, client_addr;
 
 int main() {
+	char* buffer[256];
 	int skt = socket(AF_INET, SOCK_STREAM, 0);
 	
 	if (skt < 0){
@@ -32,6 +32,30 @@ int main() {
 	
 	listen(skt, 1);
 	std::cout << "Server listening on port " << PORT;
+	
+	int newskt = accept(skt, (struct sockaddr*)&client_addr, (socklen_t*)sizeof(client_addr));
+	
+	if (newskt < 0) {
+		perror("Erron on accpet client");
+		exit(1);
+	} 
+	
+	bzero(buffer, 256);
+	
+	int sktread = read(newskt, buffer, 255);
+	
+	if (sktread < 0) {
+		std::cout << "Error reading message";
+		exit(1);
+	}
+	
+	std::cout << "The message recieved is: " << buffer << std::endl;
+
+	if ((write(newskt, "I recieve your message properly", 31)) < 0) {
+		std::cout << "Error writing message" << std::endl;
+		exit(1);
+	}
+	
 	
 	return std::cin.get();
 }
